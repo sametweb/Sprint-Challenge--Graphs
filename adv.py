1330ae23bd9fad6c03e40af116e8e745f2cd655c
sametweb <sametmutevellioglu@gmail.com>
2020-08-14 07:22:54 -0700

    solution

--- adv.py
+++ adv.py
@@ -17,7 +17,7 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -30,6 +30,79 @@ player = Player(world.starting_room)
 traversal_path = []
 
 
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.stack)
+
+
+def reverse(d):
+    if d == "w":
+        return "e"
+    if d == "n":
+        return "s"
+    if d == "s":
+        return "n"
+    if d == "e":
+        return "w"
+    if d == None:
+        return None
+
+
+graph = {}
+
+
+def explore(came_from=None):
+    to_visit = Stack()
+
+    if player.current_room.id not in graph:
+        graph[player.current_room.id] = {}
+
+    if came_from is not None:
+        graph[player.current_room.id][reverse(
+            came_from)] = player.current_room.get_room_in_direction(reverse(came_from)).id
+
+    for direction in player.current_room.get_exits():
+        if direction not in graph[player.current_room.id]:
+            graph[player.current_room.id][direction] = '?'
+
+    for direction in player.current_room.get_exits():
+        adj_room = player.current_room.get_room_in_direction(direction).id
+
+        if adj_room not in graph or graph[player.current_room.id][direction] == '?':
+            to_visit.push(direction)
+
+    while to_visit.size() > 0:
+
+        go_to = to_visit.pop()
+
+        if player.current_room.get_room_in_direction(go_to).id not in graph:
+            traversal_path.append(go_to)
+            graph[player.current_room.id][go_to] = player.current_room.get_room_in_direction(
+                go_to).id
+            player.travel(go_to)
+            explore(go_to)
+
+            if len(graph) == len(world.rooms):
+                return
+
+            traversal_path.append(reverse(go_to))
+            player.travel(reverse(go_to))
+
+
+print(explore())
+
 
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
@@ -41,22 +114,22 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
